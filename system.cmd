2e5300bc997a0ee427a1ba988df2a8a106a295dc
bashx00 <littlewinxs07@gmail.com>
2024-05-11 16:47:25 +0200

    Final Script Bash

--- system.cmd
+++ system.cmd
@@ -5,11 +5,13 @@ winget install --id=VirusTotal.VirusTotalUploader -e -h --scope "machine"
 rem Browsers
 winget install --id=Mozilla.Firefox -e -h --scope "machine"
 
-rem LibreOffice, Obsidian, PDF, Notes
+
+rem LibreOffice, Notes
 winget install --id=TheDocumentFoundation.LibreOffice -e -h --scope "machine"
 winget install --id=Obsidian.Obsidian -e -h --scope "machine"
 
 rem Developments
 winget install --id=Git.Git -e -h --scope "machine"
+winget install --id=VSCodium.VSCodium -e -h --scope "machine"
 winget install --id=Xmind.Xmind -e -h --scope "machine"
 winget install --id=OpenJS.NodeJS -e -h --scope "machine"
