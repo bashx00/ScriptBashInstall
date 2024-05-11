e1efe4361df0f2812cc3fa9684c316def492bb3f
bashx00 <littlewinxs07@gmail.com>
2024-05-11 15:46:47 +0200

    Update script

--- system.cmd
+++ system.cmd
@@ -1,22 +1,17 @@
-rem securité et système
-winget install --id=DominikReichl.KeePass -e -h --scope "machine" 
+rem Security and System
+winget install --id=DominikReichl.KeePass -e -h --scope "machine"
 winget install --id=VirusTotal.VirusTotalUploader -e -h --scope "machine"
 
-rem outils (navigateurs)
-winget install --id=Google.Chrome -e -h --scope "machine" 
-winget install --id=Mozilla.Firefox -e -h --scope "machine" 
+rem Browsers
+winget install --id=Mozilla.Firefox -e -h --scope "machine"
 
-rem outils (communication)
-winget install --id=Discord.Discord -e -h --scope "machine"
-winget install --id=Telegram.TelegramDesktop -e -h --scope "machine"
 
-rem applications (LibreOffice, Obsidian, PDF)
+rem LibreOffice, Obsidian, PDF, Notes
 winget install --id=TheDocumentFoundation.LibreOffice -e -h --scope "machine"
 winget install --id=Obsidian.Obsidian -e -h --scope "machine"
-winget install --id=Adobe.Acrobat.Reader.64-bit -e -h --scope "machine"
 
-rem outils (niveau Développements)
+
+rem Developments
 winget install --id=Git.Git -e -h --scope "machine"
-winget install --id=VSCodium.VSCodium -e -h --scope "machine"
 winget install --id=Xmind.Xmind -e -h --scope "machine"
 winget install --id=OpenJS.NodeJS -e -h --scope "machine"
